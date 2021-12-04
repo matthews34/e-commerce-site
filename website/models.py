11b5e031157c8ebb22ad257baf5163267c88ab2b
Matheus Vieira <matheus.sousav@gmail.com>
2021-12-04 17:54:03 -0300

    Create tables

--- website/models.py
+++ website/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    address = models.CharField(max_length=256)
+
+class Sale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    date = models.DateTimeField()
+    value = models.FloatField()
     
     
