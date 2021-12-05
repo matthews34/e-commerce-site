20ec5a87ab5a2975cc131f50bd0fbbee84b92db0
Matheus Vieira <matheus.sousav@gmail.com>
2021-12-05 18:33:50 -0300

    Add catalog and buy page

--- website/urls.py
+++ website/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('product/register/', views.register_product, name='register_product'),
     path('product/list/', views.list_products, name='list_products'),
-    path('product/<int:id>', views.detail_product, name='datail_product'),
+    path('product/<int:id>/', views.detail_product, name='datail_product'),
     path('product/<int:id>/update/', views.update_product, name='update_product'),
-    path('product/<int:id>/delete/', views.delete_product, name='delete_product')
+    path('product/<int:id>/delete/', views.delete_product, name='delete_product'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('product/<int:product_id>/buy/', views.buy_product, name='buy_product')
 ]
--- website/views.py
+++ website/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import View
+from django.utils import timezone
+from django.db.models import F
 
 from .forms import ProductForm
-from .models import Product
+from .models import Product, User, Sale
 
 # Create your views here.
 
@@ -35,7 +37,7 @@ def detail_product(request, id):
 
 def update_product(request, id):
     product = get_object_or_404(Product, id=id)
-    form = ProductForm(request.POST or None, instance=product)
+    form = ProductForm(request.POST or None, request.FILES or None, instance=product)
 
     if form.is_valid():
         form.save()
@@ -53,13 +55,49 @@ def delete_product(request, id):
     context = {
         'product': product
     }
-
     if request.method == 'POST':
         product.delete()
         return HttpResponseRedirect(reverse('website:list_products'))
 
     return render(request, 'website/delete_product.html')
 
+def catalog(request):
+    products = get_list_or_404(Product, stock__gt=0)
+    context = {
+        'products': products
+    }
+    return render(request, 'website/catalog.html', context)
+
+def buy_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    context = {
+        'product': product
+    }
+
+    if request.method == 'POST':
+        # validate stock
+        try:
+            quantity = int(request.POST['quantity'])
+        except ValueError:
+            return HttpResponse('Quantidade deve ser um número inteiro.', status=400)
+        if quantity > product.stock:
+            return HttpResponse('Não há quantidade suficiente em estoque.', status=400)
+        # get user
+        user = get_object_or_404(User, email=request.POST['user_email'])
+        sale = Sale(
+            product=product,
+            user=user,
+            quantity=quantity,
+            date=timezone.now(),
+            value=product.price * quantity
+        )
+        sale.save()
+        # reduce stock
+        product.stock = F('stock') - quantity
+        product.save()
+        return HttpResponse('Success')
+    
+    return render(request, 'website/buy_product.html', context)
         
 def index(request):
     return HttpResponse('Index')
