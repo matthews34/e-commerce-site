d047a765063347a9fa87eaf04ff031039f25507c
Matheus Vieira <matheus.sousav@gmail.com>
2021-12-05 18:50:20 -0300

    Alter database fields
    Change integer fields to positive integer.
    Add validators to float fields to validate only positive numbers.
    Add "unique" constraint to user email.

--- website/models.py
+++ website/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=128)
     category = models.CharField(max_length=128)
-    price = models.FloatField()
-    stock = models.IntegerField()
+    price = models.FloatField(validators=[MinValueValidator(0)])
+    stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
@@ -13,14 +14,14 @@ class Product(models.Model):
 
 class User(models.Model):
     name = models.CharField(max_length=128)
-    email = models.CharField(max_length=128)
+    email = models.CharField(max_length=128, unique=True)
     address = models.CharField(max_length=256)
 
 class Sale(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     date = models.DateTimeField()
-    value = models.FloatField()
+    value = models.FloatField(validators=[MinValueValidator(0)])
     
     
